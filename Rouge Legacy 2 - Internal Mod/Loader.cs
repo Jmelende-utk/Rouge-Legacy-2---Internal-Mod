314034a722daa56c176766e517afc58f2b778c87
Jmelende-utk <jmelende@vols.utk.edu>
2022-05-03 20:41:00 -0400

    Add project files.

---
+++ Rouge Legacy 2 - Internal Mod/Loader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Rouge_Legacy_2___Internal_Mod
+{
+    public class Loader
+    {
+        public static void Init()
+        {
+            Loader.Load = new GameObject();
+            Loader.Load.AddComponent<Mods>();
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
+        }
+        
+        public static void Unload()
+        {
+            UnloadLoad();
+        }
+
+        private static void UnloadLoad()
+        {
+            GameObject.Destroy(Load);
+        }
+
+        private static GameObject Load;
+    }
+}
---
+++ Rouge Legacy 2 - Internal Mod/Mods.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Rouge_Legacy_2___Internal_Mod
+{
+    class Mods : MonoBehaviour
+    {
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+    }
+}
